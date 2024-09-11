f2bf077e44ff34168d17e321f0cc43a13cc5287d
siddhantsachan <149047311+siddhantsachan@users.noreply.github.com>
2024-09-11 23:33:10 +0530

    Delete webd_gdsc/models directory

--- webd_gdsc/models/questionModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const questionSchema = new mongoose.Schema({
-    year: { type: Number, required: true },
-    category: { type: String, required: true },
-    content: { type: String, required: true }
-});
-
-module.exports = mongoose.model('Question', questionSchema);
--- webd_gdsc/models/submissionModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const submissionSchema = new mongoose.Schema({
-    studentId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    content: { type: String, required: true },
-    status: { type: String, enum: ['pending', 'accepted', 'rejected'], default: 'pending' }
-});
-
-module.exports = mongoose.model('Submission', submissionSchema);
